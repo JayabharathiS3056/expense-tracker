925816b8f082d3e66ae67d71dcb9ff937c639da4
JayabharathiS3056 <jayabharathis3056@gmail.com>
2026-05-11 16:37:28 +0530

    CORS update for production

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
